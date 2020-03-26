6411d14f5bc79fca40f39964d7839c00b07eff38
FrankieFigz <fjvannini@gmail.com>
2020-03-26 13:47:58 -0400

    feat: Detect email not found and display error message

--- public/assets/js/user-login-logic.js
+++ public/assets/js/user-login-logic.js
@@ -10,20 +10,25 @@ $(document).ready(function(){
         let passwordInput = $("#password-input").val().trim();
         $.get("/api/login/" + userNameInput)
         .then(function(result){
-            passwordStoredInDB = result[0].password;
-            userId = result[0].id;
-            if(passwordValid(passwordStoredInDB,passwordInput)){                
-                // The retrieved ID of the user is saved in session storage
-                // This will later be used to handle matching and saving user answers to the survey
-                sessionStorage.setItem("user_id",userId);
-                window.location.replace("/index");
-            } else {
-                $(".error-message").text("The username/password combination does not exist. Please try again.");
-            };
-        }).catch(function(err){
-            $(".error-message").text("The username/password does not exist. Please try again.");
-            throw err;
-        });
+
+            if(result.length != 0) {
+                passwordStoredInDB = result[0].password;
+                userId = result[0].id;
+                if(passwordValid(passwordStoredInDB,passwordInput)){                
+                    // The retrieved ID of the user is saved in session storage
+                    // This will later be used to handle matching and saving user answers to the survey
+                    sessionStorage.setItem("user_id",userId);
+                    window.location.replace("/index");
+                } else {
+                    // Case where the password does NOT match with the password in the DB
+                    $(".error-message").text("The username/password combination does not exist. Please try again.");
+                };
+            } else{
+                // Case where the username is not found in the DB
+                $(".error-message").text("The username/password does not exist. Please try again.");
+            } 
+
+        })
     };
     // User login on click function
     $("#login").on("click", function(){
